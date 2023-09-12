f86d5d39badc3c7e61a144cf53a2ec0a17bd177a
lql <1964757607@qq.com>
2023-09-12 17:49:45 +0800

    feat(utils):SeparateWordUtils分词工具类添加过滤掉标点符号的作用

--- 3121004869/duplicate-check/src/main/java/com/lql/www/utils/SeparateWordUtils.java
+++ 3121004869/duplicate-check/src/main/java/com/lql/www/utils/SeparateWordUtils.java
@@ -2,8 +2,12 @@ package com.lql.www.utils;
 
 import com.huaban.analysis.jieba.JiebaSegmenter;
 
+import java.io.*;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 分词工具类
@@ -11,7 +15,33 @@ import java.util.Set;
  * @author lql 2023/09/11
  */
 public class SeparateWordUtils {
+    /**
+     * 分词库
+     */
     private final JiebaSegmenter segmenter = new JiebaSegmenter();
+
+    /**
+     * 常用词以及标点符号的数组
+     */
+    private List<String> normalWordList;
+
+    {
+        // 初始化常用词数组
+        normalWordList = new LinkedList<>();
+        // 读取常用词及标点符号的文件
+        InputStream is = this.getClass().getResourceAsStream("/stop_words.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line;
+        try {
+            // 将读取的内容添加到集合中
+            while ((line = reader.readLine()) != null) {
+                normalWordList.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("error: 读取文件出错");
+        }
+    }
+
 
     /**
      * @param text 输入文本
@@ -19,7 +49,11 @@ public class SeparateWordUtils {
      */
     public Set<String> separateWord(String text){
         Set<String> set = new HashSet<>();
-        if(!set.addAll(segmenter.sentenceProcess(text))){
+        // 分词
+        List<String> result = segmenter.sentenceProcess(text);
+        // 过滤掉标点符号
+        result = result.stream().map(String::trim).filter(o -> !normalWordList.contains(o)).collect(Collectors.toList());
+        if(!set.addAll(result)){
             System.out.println("分词失败");
         }
         return set;

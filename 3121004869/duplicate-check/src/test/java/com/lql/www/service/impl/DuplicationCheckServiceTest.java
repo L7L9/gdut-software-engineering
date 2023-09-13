c2b78061262af21333c5c6478a7c1324cb7dcce2
lql <1964757607@qq.com>
2023-09-13 10:41:46 +0800

    feat(test):更新测试用例

--- 3121004869/duplicate-check/src/test/java/com/lql/www/service/impl/DuplicationCheckServiceTest.java
+++ 3121004869/duplicate-check/src/test/java/com/lql/www/service/impl/DuplicationCheckServiceTest.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Date;
 
 /**
+ * DuplicationCheckService对应的测试类
+ *
  * @author lql 2023/09/11
  */
 public class DuplicationCheckServiceTest {
@@ -24,6 +26,22 @@ public class DuplicationCheckServiceTest {
                 "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\orig_0.8_dis_10.txt",
                 "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\orig_0.8_dis_15.txt"
         };
+//        String origin = "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin.txt";
+//        String output = "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\result.txt";
+//
+//        String[] targetFiles = {
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_1.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_2.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_3.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_4.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_5.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_6.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_7.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_8.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_9.txt",
+//                "D:\\school-3\\软件工程\\gdut-software-engineering\\3121004869\\duplicate-check\\src\\test\\resources\\another_test\\origin_10.txt"
+//        };
+
         // 调用duplicateCheckService接口
         for (String target:targetFiles) {
             double result = duplicateCheckService.duplicateCheck(origin,target,output);
